d832f4e29b56b2fbd4f0714e3024f6cf58e96108
Mihail-Eugen PASCU (78094) <mihail_eugen.pascu@stud.acs.upb.ro>
2020-02-05 08:41:34 +0200

    Sync the wake time with an NTP Server

--- DailyQuotes.ino
+++ DailyQuotes.ino
@@ -2,8 +2,11 @@ extern "C" {                      //this library is used for the RTC memory read
   #include "user_interface.h"     //
 }                                 //
 
-#include <ESP8266WiFi.h>          //include the library for the wifi connection
+#include <ESP8266WiFi.h>          //include the libraries for the wifi connection
 #include <ESP8266HTTPClient.h>    //
+
+#include <NTPClient.h>            //include the libraries for the NTP connection
+#include <WiFiUdp.h>              //
 
 #include "SPI.h"                  //include the needed library for the TFT display
 #include "Adafruit_GFX.h"         //
@@ -17,16 +20,20 @@ extern "C" {                      //this library is used for the RTC memory read
 #define TFT_RST  -1               //
 #define TFT_MISO -1               //
 
-
 #define HOUR (unsigned long)36e5     //define the values for the time to sleep (unsigned long is used as parameter for delay()
                                      //one hour in milisecond (3.600.000 miliseconds=3.600 seconds=60 minutes=1 hour)
-uint64_t sleepTimeUs = 123e8;        //123e8 for approximately 3 hours and 25 minutes
+uint64_t sleepTimeUs = 123e8;        //108e8 for approximately 3 hours and 25 minutes
                                      //1e6   for 1 second
 uint32_t sleepCounter;               //the sleep counter is used to make the ESP8266 sleep for 24 hours
-                                     //ESP8266 can deepSleep for maximum 3 hours and 25 minutes
+                                     //ESP8266 can deepSleep for maximum 3 hours
                                      //so we put the ESP8266 to deepSleep 7 times
                                      //3*7=21hours and 25*7=175minutes=2hour, in total 23 hours of deepSleep
 
+const long utcOffsetInSeconds = 7200;                                       //offset for Romania hour
+uint32_t timeSaver;                                                         //used to store the time when the qoute must be displayed, in this we save hours (uses 5 bits) and minutes(uses 6 bits)
+byte hours,minutes;                                                         //used to take data from rtc memory
+WiFiUDP ntpUDP;                                                             //define the NTP client to get time
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", utcOffsetInSeconds);    //
 
 const char* ssid = "yourSSID";          //the SSID data
 const char* password = "yourPassword";  //    
@@ -39,6 +46,8 @@ void setup() {
   wifi_set_sleep_type(LIGHT_SLEEP_T);       //Set the lightSleep mode when the delay() function is called
   
   checkSleepCounter();
+
+  syncWithNTPServer(); //sync with the time from internet (display the qoute at exact hour and minute)
   
   turnDisplayOn();
   
@@ -59,6 +68,7 @@ void setup() {
   displayQuote();
 
   initializeSleepCounter();
+  storeCurrentTime();
   delay(HOUR);                                    //light sleep for one hour
   
   goSleep();
@@ -105,6 +115,23 @@ void initializeSleepCounter(){
   system_rtc_mem_write(68,&sleepCounter,4);
 }
 
+void storeCurrentTime(){
+  timeClient.begin();
+  delay(1000);
+  timeClient.update();
+  timeSaver=timeClient.getHours()<<6;   //store minutes and hours in the same variable
+                                        //for hours we need only 5 bits (2^5=32 > 24 max hour value)
+  timeSaver|=timeClient.getMinutes();   //for minutes we need 6 bits (2^6=64 > 60 max minute value)
+  system_rtc_mem_write(72,&timeSaver,4);
+  timeClient.end();
+}
+
+void restorePreviousTime(){
+  system_rtc_mem_read(72,&timeSaver,4);
+  hours=timeSaver>>6;
+  minutes=timeSaver&0x3F;
+}
+
 void connectWiFi(){
   WiFi.begin(ssid, password);                                               //Try to connect to the WiFi
   tft.print("Connecting");                                                  //
@@ -148,5 +175,33 @@ void displayQuote(){
       tft.println(author);                                                    //
     }
     http.end();                                                               //Close connection
+  }
+}
+
+void syncWithNTPServer(){
+  if( sleepCounter == 0 ) {
+    restorePreviousTime();                                                    //Get previous time from RTC user memory
+    Serial.begin(115200);                                                     //Used for debugging
+    Serial.println("Connecting to WiFi");
+    WiFi.begin(ssid, password);                                               //Try to connect to the WiFi
+    while (WiFi.status() != WL_CONNECTED) {                                   //
+      Serial.print(".");                                                      //
+      delay(500);                                                             //
+    }                                                                         //
+    Serial.end();
+    timeClient.begin();
+    delay(1000);
+    timeClient.update();
+    WiFi.disconnect();
+    if( timeClient.getHours() < hours ){                                                                                                       //if the current hour is less then the wake hour
+       ESP.deepSleep( ((hours-timeClient.getHours())*60*60*1000*1000) + ((minutes-timeClient.getMinutes())*60*1000*1000), WAKE_RF_DEFAULT);    //go to deepsleep
+    }
+    if( timeClient.getHours() == hours ){
+      if( (timeClient.getMinutes()- minutes > -5) && (timeClient.getMinutes()-minutes < 0) ){ //if there are less than 5 minutes 
+        delay((minutes-timeClient.getMinutes())*60*1000);                                     //light sleep for that time and then display quote
+      }else{                                                                                  //else
+        ESP.deepSleep( ((minutes-timeClient.getMinutes())*60*1000*1000), WAKE_RF_DEFAULT);    //go to deepsleep for the remaining time
+      }
+    }
   }
 }
